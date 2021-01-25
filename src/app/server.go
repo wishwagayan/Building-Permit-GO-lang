ba7b98ff993f29e2d054de89f0b0e602ded90425
Wishwa Gayan Wijayasinghe <vishva@outlook,com>
2021-01-25 14:12:47 +0530

    files adding

--- src/app/server.go
+++ src/app/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"go-scraper/src/spider"
 	"log"
 	"net/http"
 )
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	log.Println("Ping..")
-	w.Write([]byte("ping"))
+	w.Write([]byte("ping sucessfully"))
 }
 
 func runServer() {
 	addr := ":7171"
 	http.HandleFunc("/ping", ping)
+	log.Println("listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func main() {
+	spider.Phase()
 }
